781b1a7418eeb13dd2b532b74428ad43aa479f9d
fdy200533 <fdy200533@126.com>
2022-03-13 23:32:01 +0800

    update 20220313

--- samples_fdy/demo.py
+++ samples_fdy/demo.py
@@ -1,4 +1,3 @@
-import mmdet
 from mmdet.apis import init_detector, inference_detector
 
 config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
---
+++ samples_fdy/train.sh
@@ -0,0 +1,9 @@
+# python -u tools/train.py \
+#     configs_fdy/exps/fcos/20220103_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_1x_coco.py \
+#     --gpus 1 \
+#     --work-dir work_dirs/20220103_fcos_center_dcn 
+
+python -u tools/train.py \
+    configs_fdy/exps/fcos/20220313_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_1x_voc.py \
+    --gpus 1 \
+    --work-dir work_dirs/20220313_fcos_center_dcn_voc
