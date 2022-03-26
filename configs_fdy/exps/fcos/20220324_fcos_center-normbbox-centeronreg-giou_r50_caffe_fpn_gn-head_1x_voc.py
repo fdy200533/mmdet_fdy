ca750b8b0ddecd416cf25df4cc05c972f1d6c291
fdy200533 <fdy200533@126.com>
2022-03-26 11:30:49 +0800

    update 20220326

--- configs_fdy/exps/fcos/20220324_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_voc.py
+++ configs_fdy/exps/fcos/20220324_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_voc.py
@@ -9,8 +9,6 @@ model = dict(
                   norm_cfg=dict(type='BN', requires_grad=False),
                   norm_eval=True,
                   style='caffe',
-                  dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
-                  stage_with_dcn=(False, True, True, True),
                   init_cfg=dict(type='Pretrained', checkpoint='open-mmlab://detectron2/resnet50_caffe')
                   # checkpoint='checkpoints/resnet50_msra-5891d200.pth')
                   ),
@@ -31,7 +29,7 @@ model = dict(
                    loss_cls=dict(type='FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=1.0),
                    norm_on_bbox=True,
                    centerness_on_reg=True,
-                   dcn_on_last_conv=True,
+                   dcn_on_last_conv=False,
                    center_sampling=True,
                    conv_bias=True,
                    loss_bbox=dict(type='GIoULoss', loss_weight=1.0),
@@ -48,11 +46,40 @@ model = dict(
     test_cfg=dict(nms_pre=1000,
                   min_bbox_size=0,
                   score_thr=0.05,
-                  nms=dict(type='nms', iou_threshold=0.6),
+                  nms=dict(type='nms', iou_threshold=0.5),
                   max_per_img=100))
 
 # dataset settings
-data = dict(samples_per_gpu=8, workers_per_gpu=4, train=dict(type='RepeatDataset', times=1))
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(800, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='MultiScaleFlipAug',
+         img_scale=(800, 600),
+         flip=False,
+         transforms=[
+             dict(type='Resize', keep_ratio=True),
+             dict(type='RandomFlip'),
+             dict(type='Normalize', **img_norm_cfg),
+             dict(type='Pad', size_divisor=32),
+             dict(type='ImageToTensor', keys=['img']),
+             dict(type='Collect', keys=['img']),
+         ])
+]
+data = dict(samples_per_gpu=8,
+            workers_per_gpu=4,
+            train=dict(type='RepeatDataset', times=1, dataset=dict(pipeline=train_pipeline)),
+            val=dict(pipeline=test_pipeline),
+            test=dict(pipeline=test_pipeline))
 
 # optimizer
 optimizer = dict(type='SGD',
@@ -63,7 +90,7 @@ optimizer = dict(type='SGD',
 optimizer_config = dict(grad_clip=None)
 
 # learning policy
-lr_config = dict(policy='step', warmup='linear', warmup_iters=500, warmup_ratio=1.0 / 3, step=[14])
-runner = dict(type='EpochBasedRunner', max_epochs=16)
+lr_config = dict(policy='step', warmup='linear', warmup_iters=500, warmup_ratio=1.0 / 3, step=[9])
+runner = dict(type='EpochBasedRunner', max_epochs=12)
 
 fp16 = dict(loss_scale=512.)
--- samples_fdy/train.sh
+++ samples_fdy/train.sh
@@ -3,7 +3,12 @@
 #     --gpus 1 \
 #     --work-dir work_dirs/20220103_fcos_center_dcn 
 
+# python -u tools/train.py \
+#     configs_fdy/exps/fcos/20220313_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_1x_voc.py \
+#     --gpus 1 \
+#     --work-dir work_dirs/20220313_fcos_center_dcn_voc
+
 python -u tools/train.py \
-    configs_fdy/exps/fcos/20220313_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_dcn_1x_voc.py \
+    configs_fdy/exps/fcos/20220324_fcos_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_1x_voc.py \
     --gpus 1 \
-    --work-dir work_dirs/20220313_fcos_center_dcn_voc
+    --work-dir work_dirs/20220324_fcos_center_voc
